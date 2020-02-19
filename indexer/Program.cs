90d970735dff6d4d11b52ecb97a7263ba9288766
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:48:32 +0530

    Initial commit

---
+++ indexer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Indexdemo()
+        {
+
+        }
+      stdetails[1]=new wstudent
+            {
+            ID=777;
+            StudentName="rohitha";
+            Course="Webapps";
+    };
+    Console.WriteLine("{0}\t{1}\t{2}",stdetails[0].ID,
+        stdetails[0].StudenbtName,
+        stdetails[0].Course);
+        Console.Writteline("{0}\t{1}\t{2}",stdetails[1].Id,
+        stdetails[1].StudentName,
+        stdetails[1].Course);
+
+}
